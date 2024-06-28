5db87ed8c65d87abf24d45a24939ee0112b44474
nh <95534164+ellie21520813@users.noreply.github.com>
2024-06-29 03:47:36 +0700

    nh

--- config/cache.php
+++ config/cache.php
@@ -51,7 +51,7 @@ return [
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache/data'),
+            'path' => env('CACHE_PATH', storage_path('framework/cache')),
         ],
 
         'memcached' => [
